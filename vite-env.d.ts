7ccbeae18cf7fab04cfb66ee9426527d2daee3d6
DannyAC1 <danny.asc@gmail.com>
2025-12-07 15:13:37 -0500

    App completa y funcional

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_KEY: string;
+    readonly [key: string]: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
+  declare namespace NodeJS {
+    interface ProcessEnv {
+      API_KEY: string;
+      [key: string]: string | undefined;
+    }
+  }
+  
